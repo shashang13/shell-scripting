99e1cc3de4604313ca0dfed199869e58b5c79f84
shashang13 <shashang13@gmail.com>
2022-09-18 23:06:19 +0530

    new line and tab character

--- 02-print.sh
+++ 02-print.sh
@@ -14,3 +14,9 @@ echo -e "\e[31mHello, this is my first line in color\e[0m"
 # ""      => Mandatory for color to work
 # \e[colm => Enables color of mentioned code.
 # \e[0m   => Disables color of the target line
+
+# \n      => New line character
+# \t      => Tab character
+
+echo -e "Line1\n\nLine2"
+echo -e "Word1\t\tWord2"
