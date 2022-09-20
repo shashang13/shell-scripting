a19127ec828993623248bbc218aae551045d11d8
shashang13 <shashang13@gmail.com>
2022-09-20 13:55:03 +0530

    Providing steps information

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -5,15 +5,21 @@
 #  echo You need to be root user to execute this program
 #fi
 
+echo "Installing NGINX"
 yum install nginx -y
 
+echo "Downloading Frontend software"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
+echo "Cleanup Old Nginx Content"
 cd /usr/share/nginx/html
 rm -rf *
+
+echo "Extracting Archive"
 unzip /tmp/frontend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
 
+echo "Starting Nginx"
 systemctl enable nginx
 systemctl start nginx
