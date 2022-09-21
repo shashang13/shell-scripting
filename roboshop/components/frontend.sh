1dc0f78b0672a41c48e64e0f35b272c165ca7089
shashang13 <shashang13@gmail.com>
2022-09-21 12:27:02 +0530

    Improving code through status check function

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -6,51 +6,39 @@ if [ ${USER_ID} -ne 0 ]; then
   exit 1
 fi
 
+STATUS_CHECK (){
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSUCCESS\e[0m"
+  else
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 2
+  fi
+}
+
 echo -e "\e[36mInstalling NGINX\e[0m"
 yum install nginx -y
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+STATUS_CHECK
+
 echo ''
 echo -e "\e[36mDownloading Frontend software\e[0m"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+STATUS_CHECK
+
 echo ''
 echo -e "\e[36mCleanup Old Nginx Content\e[0m"
 cd /usr/share/nginx/html
 rm -rf *
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+STATUS_CHECK
+
 echo ''
 echo -e "\e[36mExtracting Archive\e[0m"
 unzip /tmp/frontend.zip
 mv frontend-main/static/* .
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+STATUS_CHECK
+
 echo ''
 echo -e "\e[36mStarting Nginx\e[0m"
 systemctl enable nginx
 systemctl start nginx
-if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
-else
-  echo -e "\e[31mFAILURE\e[0m"
-  exit 2
-fi
+STATUS_CHECK
