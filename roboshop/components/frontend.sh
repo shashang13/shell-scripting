a0469b03392cd7a10d4b18fe5201bf58d80c19a7
shashang13 <shashang13@gmail.com>
2022-09-21 13:07:59 +0530

    Implemented Print function

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -7,11 +7,11 @@ if [ ${USER_ID} -ne 0 ]; then
 fi
 
 statusCheck (){
-  if [ $? -eq 0 ]; then
+  if [ $1 -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
     echo -e "\e[31mFAILURE\e[0m"
-    exit 3
+    exit $1
   fi
 }
 
@@ -21,23 +21,23 @@ descriptionPrint () {
 
 descriptionPrint 'Installing NGINX'
 yum install nginx -y
-statusCheck
+statusCheck $?
 
 descriptionPrint 'Downloading Frontend Software'
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
-statusCheck
+statusCheck $?
 
 descriptionPrint 'Cleanup Old Nginx Content'
 rm -rf /usr/share/nginx/html/*
-statusCheck
+statusCheck $?
 
 descriptionPrint 'Extracting Archive'
 unzip /tmp/frontend.zip
 mv frontend-main/static/* .
 m frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-statusCheck
+statusCheck $?
 
 descriptionPrint "Starting Nginx"
 systemctl enable nginx
 systemctl start nginx
-statusCheck
+statusCheck $?
