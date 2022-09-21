355f561a619e20eca68a7d7cea219200dae2f6d6
shashang13 <shashang13@gmail.com>
2022-09-21 13:41:12 +0530

    Redirecting outputs to logfile.

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -25,7 +25,7 @@ logFile=/tmp/roboshop.log
 rm -f $logFile
 
 descriptionPrint 'Installing NGINX'
-yum install nginx -y
+yum install nginx -y  >> $logFile
 statusCheck $?
 
 descriptionPrint 'Downloading Frontend Software'
@@ -33,17 +33,17 @@ curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/f
 statusCheck $?
 
 descriptionPrint 'Cleanup Old Nginx Content'
-cd /usr/share/nginx/html/ && rm -rf /usr/share/nginx/html/*
+cd /usr/share/nginx/html/ >>$logFile && rm -rf /usr/share/nginx/html/* >>$logFile
 statusCheck $?
 
 descriptionPrint 'Extracting Archive'
-unzip -o /tmp/frontend.zip && mv frontend-main/static/* .
+unzip -o /tmp/frontend.zip >>$logFile && mv frontend-main/static/* . >>$logFile
 statusCheck $?
 
 descriptionPrint 'Update Roboshop Configuration'
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf >>$logFile
 statusCheck $?
 
 descriptionPrint "Starting Nginx"
-systemctl enable nginx && systemctl start nginx
+systemctl enable nginx >>$logFile && systemctl start nginx >>$logFile
 statusCheck $?
