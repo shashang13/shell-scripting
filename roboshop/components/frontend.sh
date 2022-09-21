bcf5bfa170391cd7b8467ee68858b67da7c90e6d
shashang13 <shashang13@gmail.com>
2022-09-21 15:09:59 +0530

    corrected frontend.sh typo

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,19 +3,19 @@
 source components/common.sh
 
 descriptionPrint 'Installing NGINX'
-yum install nginx -y  >> $logFile
+yum install nginx -y  &>> $logFile
 statusCheck $?
 
 descriptionPrint 'Downloading Frontend Software'
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> $logFile
 statusCheck $?
 
 descriptionPrint 'Cleanup Old Nginx Content'
-cd /usr/share/nginx/html/ >>$logFile && rm -rf /usr/share/nginx/html/* >>$logFile
+cd /usr/share/nginx/html/ &>>$logFile && rm -rf /usr/share/nginx/html/* &>>$logFile
 statusCheck $?
 
 descriptionPrint 'Extracting Archive'
-unzip -o /tmp/frontend.zip >>$logFile && mv frontend-main/static/* . >>$logFile
+unzip -o /tmp/frontend.zip &>>$logFile && mv frontend-main/static/* . &>>$logFile
 statusCheck $?
 
 descriptionPrint 'Update Roboshop Configuration'
@@ -23,5 +23,5 @@ mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$logFile
 statusCheck $?
 
 descriptionPrint "Starting Nginx"
-systemctl enable nginx >>$logFile && systemctl start nginx >>$logFile
+systemctl enable nginx &>>$logFile && systemctl start nginx &>>$logFile
 statusCheck $?
