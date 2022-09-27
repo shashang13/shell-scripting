4fe7903fd3784ab644cf96d5f0f345ff269da7e6
shashang13 <shashang13@gmail.com>
2022-09-27 07:37:05 +0530

    common.sh - updated stage status in statusCheck

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,20 +8,20 @@ statusCheck $? "${STAGE}"
 
 descriptionPrint 'Downloading Frontend Software'
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>> $logFile
-statusCheck $?
+statusCheck $? "${STAGE}"
 
 descriptionPrint 'Cleanup Old Nginx Content'
 cd /usr/share/nginx/html/ &>>$logFile && rm -rf /usr/share/nginx/html/* &>>$logFile
-statusCheck $?
+statusCheck $? "${STAGE}"
 
 descriptionPrint 'Extracting Archive'
 unzip -o /tmp/frontend.zip &>>$logFile && mv frontend-main/static/* . &>>$logFile
-statusCheck $?
+statusCheck $? "${STAGE}"
 
 descriptionPrint 'Update Roboshop Configuration'
 mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$logFile
-statusCheck $?
+statusCheck $? "${STAGE}"
 
 descriptionPrint "Starting Nginx"
 systemctl enable nginx &>>$logFile && systemctl start nginx &>>$logFile
-statusCheck $?
+statusCheck $? "${STAGE}"
