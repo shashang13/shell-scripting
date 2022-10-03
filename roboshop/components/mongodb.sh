9eaa0c447e9637b3ee2fcc757c55a61887a1a4fe
shashang13 <shashang13@gmail.com>
2022-10-03 09:19:46 +0530

    common.sh - updated stage status in statusCheck

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -3,35 +3,30 @@ source components/common.sh
 
 descriptionPrint 'Setup MongoDB repos'
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>> $logFile
-statusCheck $?
+statusCheck $? "${STAGE}"
 
 
 descriptionPrint 'Install MongoDB'
 yum install -y mongodb-org &>> $logFile
+statusCheck $? "${STAGE}"
 
 descriptionPrint 'Update Mongodb config file'
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $logFile
-statusCheck $?
+statusCheck $? "${STAGE}"
 
 descriptionPrint 'Starting Service'
 systemctl enable mongod &>> $logFile && systemctl restart mongod &>>$logFile
-statusCheck $?
+statusCheck $? "${STAGE}"
 
 
-#
-##restart the service
-#systemctl restart mongod
-#statusCheck $?
+descriptionPrint 'MongoDB Schema Download'
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+statusCheck $? "${STAGE}"
 
-#Every Database needs the schema to be loaded for the application to work.
-#Download the schema and load it.
+descriptionPrint 'Extract schema'
+cd /tmp &>>logFile && unzip -o mongodb.zip &>>logFile
+statusCheck $? "${STAGE}"
 
-#descriptionPrint 'Downloading the schema'
-## curl -f -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-#statusCheck $?
-#
-#descriptionPrint 'Extract schema'
-#cd /tmp && unzip -o mongodb.zip
-#
-#descriptionPrint 'Load Schema'
-#cd mongodb-main && mongo < catalogue.js && mongo < users.js
+descriptionPrint 'Load Schema'
+cd mongodb-main &>>logFile && mongo < catalogue.js && mongo < users.js
+statusCheck $? "${STAGE}"
