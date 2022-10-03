e0276341a223ec10dc338a811de3588f20115047
shashang13 <shashang13@gmail.com>
2022-10-03 09:31:35 +0530

    common.sh - updated stage status in statusCheck

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -2,31 +2,30 @@
 source components/common.sh
 
 descriptionPrint 'Setup MongoDB repos'
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>> $logFile
+curl -f -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>> $logFile
 statusCheck $? "${STAGE}"
 
 
 descriptionPrint 'Install MongoDB'
-yum install -y mongodb-org &>> $logFile
+yum install -y mongodb-org &>>${logFile}
 statusCheck $? "${STAGE}"
 
 descriptionPrint 'Update Mongodb config file'
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>> $logFile
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${logFile}
 statusCheck $? "${STAGE}"
-
-descriptionPrint 'Starting Service'
-systemctl enable mongod &>> $logFile && systemctl restart mongod &>>$logFile
-statusCheck $? "${STAGE}"
-
 
 descriptionPrint 'MongoDB Schema Download'
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
+curl -f -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${logFile}
 statusCheck $? "${STAGE}"
 
 descriptionPrint 'Extract schema'
-cd /tmp &>>logFile && unzip -o mongodb.zip &>>logFile
+cd /tmp &>>${logFile} && unzip -o mongodb.zip &>>${logFile}
 statusCheck $? "${STAGE}"
 
 descriptionPrint 'Load Schema'
-cd mongodb-main &>>logFile && mongo < catalogue.js >>logFile && mongo < users.js >>logFile
+cd mongodb-main &>>${logFile} && mongo < catalogue.js >>${logFile} && mongo < users.js >>${logFile}
+statusCheck $? "${STAGE}"
+
+descriptionPrint 'Starting Service'
+systemctl enable mongod &>>${logFile} && systemctl restart mongod &>>${logFile}
 statusCheck $? "${STAGE}"
