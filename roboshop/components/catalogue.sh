82bdafca4ad65fb9eba2dfe95be680335bad492c
shashang13 <shashang13@gmail.com>
2022-10-04 10:15:39 +0530

    Deploying User Component

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -2,42 +2,5 @@
 
 source components/common.sh
 
-descriptionPrint "Performing Prereqs for Catalogue components"
-echo "Download & Install nodejs" &>>${logFile}
-curl -fsL https://rpm.nodesource.com/setup_lts.x | bash - &>>${logFile} && echo "" &>>${logFile} && yum install nodejs -y &>>${logFile}
-statusCheck $? "${STAGE}"
-
-id ${App_User} &>>${logFile}
-if [ $? -ne 0 ]; then
-  descriptionPrint "Adding Application User"
-  useradd ${App_User} &>>${logFile}
-  statusCheck $? "${STAGE}"
-fi
-
-descriptionPrint "Download Catalogue component"
-curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${logFile}
-statusCheck $? "${STAGE}"
-
-descriptionPrint "Cleanup Old Content"
-rm -rf /home/roboshop/catalogue &>>${logFile}
-statusCheck $? "${STAGE}"
-
-descriptionPrint "Extract and Install Catalogue Application"
-cd /home/roboshop &>>${logFile} && unzip -o /tmp/catalogue.zip &>>${logFile} && mv catalogue-main catalogue &>>${logFile}
-statusCheck $? "${STAGE}"
-
-descriptionPrint "Install Application dependencies"
-cd /home/roboshop/catalogue &>>${logFile} && npm install &>>${logFile}
-statusCheck $? "${STAGE}"
-
-descriptionPrint "Fix App User Permissions"
-chown -R ${App_User}:${App_User} /home/roboshop/ &>>${logFile}
-statusCheck $? "${STAGE}"
-
-descriptionPrint "Setup SystemD file"
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/roboshop/catalogue/systemd.service &>>${logFile} && mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${logFile}
-statusCheck $? "${STAGE}"
-
-descriptionPrint "Starting Catalogue Service"
-systemctl daemon-reload &>>${logFile} && systemctl restart catalogue &>>${logFile} && systemctl enable catalogue &>>${logFile}
-statusCheck $? "${STAGE}"
+COMPONENT=catalogue
+nodeJS
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -19,7 +19,7 @@ unzip -o /tmp/frontend.zip &>>${logFile} && mv frontend-main/static/* . &>>${log
 statusCheck $? "${STAGE}"
 
 descriptionPrint 'Configure Frontend'
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>${logFile} &>>${logFile} && sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>${logFile}
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>${logFile} && sed -i -e '/catalogue/s/localhost/catalogue.roboshop.internal/' -e '/user/s/localhost/user.roboshop.internal/' -e '/cart/s/localhost/cart.roboshop.internal/' /etc/nginx/default.d/roboshop.conf &>>${logFile}
 statusCheck $? "${STAGE}"
 
 descriptionPrint "Start Nginx"
