224e21cdcea66168e5723f19e1b4f35c4e2b3642
shashang13 <shashang13@gmail.com>
2022-10-06 12:15:14 +0530

    Mysql instance

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -13,22 +13,24 @@ descriptionPrint "Start MySQL"
 systemctl enable mysqld &>>${logFile} && systemctl start mysqld &>>${logFile}
 statusCheck $?
 
+echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/rootpass.sql
+DEFAULT_ROOT_PASSWD=$(grep 'temporary password' /var/log/mysqld.log|awk '{print $NF}')
+mysql -uroot -p"${DEFAULT_ROOT_PASSWD}" </tmp/rootpass.sql
 echo 'show databases'|mysql -uroot -p'RoboShop@1' &>>${logFile}
-if [ $? -ne 0 ]; then
-  descriptionPrint "Change Default Root Password"
-  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/rootpass.sql
-  DEFAULT_ROOT_PASSWD=$(grep 'temporary password' /var/log/mysqld.log|awk '{print $NF}')
-  mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWD}" </tmp/rootpass.sql &>>${logFile}
-  statusCheck $?
-fi
-
-echo 'show plugins'|mysql -uroot -p'RoboShop@1' 2>>${logFile} | grep validate_password ${logFile}
-if [ $? -eq 0 ]; then
-  descriptionPrint "Uninstall Validate plugin"
-  echo 'uninstall plugin validate_password;' > /tmp/plugin.sql
-  mysql -uroot -p'RoboShop@1' </tmp/plugin.sql &>>${logFIle}
-  statusCheck $?
-fi
+#
+#if [ $? -ne 0 ]; then
+#  descriptionPrint "Change Default Root Password"
+#  mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWD}" </tmp/rootpass.sql &>>${logFile}
+#  statusCheck $?
+#fi
+#
+#echo 'show plugins'|mysql -uroot -p'RoboShop@1' 2>>${logFile} | grep validate_password ${logFile}
+#if [ $? -eq 0 ]; then
+#  descriptionPrint "Uninstall Validate plugin"
+#  echo 'uninstall plugin validate_password;' > /tmp/plugin.sql
+#  mysql -uroot -p'RoboShop@1' </tmp/plugin.sql &>>${logFIle}
+#  statusCheck $?
+#fi
 ### **Setup Needed for Application.**
 #
 #As per the architecture diagram, MySQL is needed by
