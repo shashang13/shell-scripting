6571dc8499000e9530e04022693f3fddc1b24d38
shashang13 <shashang13@gmail.com>
2022-10-06 10:47:59 +0530

    Mysql instance

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -2,7 +2,7 @@
 source components/common.sh
 
 descriptionPrint 'Setup MongoDB repos'
-curl -f -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>> $logFile
+curl -f -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>> ${logFile}
 statusCheck $? "${STAGE}"
 
 
