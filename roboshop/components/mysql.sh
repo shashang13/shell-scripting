74072117c13d47c1749ee82ba3cd0bda8cb7e306
shashang13 <shashang13@gmail.com>
2022-10-06 11:48:10 +0530

    Mysql instance

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -13,20 +13,20 @@ descriptionPrint "Start MySQL"
 systemctl enable mysqld &>>${logFile} && systemctl start mysqld &>>${logFile}
 statusCheck $?
 
-echo 'show databases'|mysql -uroot -p'Roboshop@1' &>>${logFile}
+echo 'show databases'|mysql -uroot -p'RoboShop@1' &>>${logFile}
 if [ $? -ne 0 ]; then
   descriptionPrint "Change Default Root Password"
-  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('Roboshop@1');" > /tmp/rootpass.sql
+  echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('RoboShop@1');" > /tmp/rootpass.sql
   DEFAULT_ROOT_PASSWD=$(grep 'temporary password' /var/log/mysqld.log|awk '{print $NF}')
   mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWD}" </tmp/rootpass.sql &>>${logFile}
   statusCheck $?
 fi
 
-echo 'show plugins'|mysql -uroot -p'Roboshop@1' >>${logFile} && grep validate_password ${logFile}
+echo 'show plugins'|mysql -uroot -p'RoboShop@1' >>${logFile} | grep validate_password ${logFile}
 if [ $? -eq 0 ]; then
   descriptionPrint "Uninstall Validate plugin"
   echo 'uninstall plugin validate_password;' > /tmp/plugin.sql
-  mysql -uroot -p'Roboshop@1' </tmp/plugin.sql &>>${logFIle}
+  mysql -uroot -p'RoboShop@1' </tmp/plugin.sql &>>${logFIle}
   statusCheck $?
 fi
 ### **Setup Needed for Application.**
