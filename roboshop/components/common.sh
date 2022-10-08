b8ea28d39dde18a138930202151a1684d9aa4b76
shashang13 <shashang13@gmail.com>
2022-10-08 12:48:13 +0530

    shipping instance

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -47,7 +47,7 @@ serviceSetup () {
          -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' \
          -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' \
          -e 's/CARTENDPOINT/cart.roboshop.internal/' \
-         -e 's/DBHOST/mysql.roboshop.internal/'
+         -e 's/DBHOST/mysql.roboshop.internal/' \
          /home/roboshop/${COMPONENT}/systemd.service &>>${logFile} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${logFile}
   statusCheck $?
 
