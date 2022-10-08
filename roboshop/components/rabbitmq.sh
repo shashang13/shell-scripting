c6b2bdaa030e180a91ccf761959b32e67fbebc0e
shashang13 <shashang13@gmail.com>
2022-10-08 17:55:30 +0530

    rabbitmq instance

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -18,6 +18,8 @@ if [ -z "$(rabbitmqctl list_users|grep roboshop)" ]; then
   descriptionPrint "Create App User"
   rabbitmqctl add_user roboshop roboshop123 &>>${logFile} && rabbitmqctl set_user_tags roboshop administrator &>>${logFile} && rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${logFile}
   statusCheck $?
+elif [ ! -z "$(rabbitmqctl list_users|grep roboshop)" ]; then
+  echo app user already available.
 fi
 
 
