60009a7ed066bcceb354cfe6ad38a1b02c078592
shashang13 <shashang13@gmail.com>
2022-10-08 16:37:16 +0530

    rabbitmq instance

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -6,7 +6,7 @@ curl -f -s -L https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-ser
 statusCheck $?
 
 descriptionPrint "Install Erlang and RabbitMQ"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v25.1/erlang-25.1-1.el8.x86_64.rpm rabbitmq-server -y &>>${logFile}
+yum --skip-broken install https://github.com/rabbitmq/erlang-rpm/releases/download/v25.1/erlang-25.1-1.el8.x86_64.rpm rabbitmq-server -y &>>${logFile}
 statusCheck $?
 
 #descriptionPrint "Install RabbitMQ"
