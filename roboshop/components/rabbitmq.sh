2492af594968e3e8fd1a55a9dad768dc62a34421
shashang13 <shashang13@gmail.com>
2022-10-08 17:23:28 +0530

    rabbitmq instance

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -2,7 +2,7 @@
 source components/common.sh
 
 descriptionPrint "Download RabbitMQ dependencies"
-curl -f -s -L https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${logFile}
+curl -f -L -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${logFile}
 statusCheck $?
 
 descriptionPrint "Install Erlang and RabbitMQ"
