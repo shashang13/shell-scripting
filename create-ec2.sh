adf8046e9e1f0aa732394938d1dba7469afb7434
shashang13 <shashang13@gmail.com>
2022-11-18 04:55:38 +0530

    create-ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Input machine name is needed"
+fi
+
+COMPONENT=$1
+
+
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice"|jq '.Images[].ImageId'|sed -e 's/"//g')
-echo $AMI_ID
-#aws ec2 run-instances --image-id ami-0abcdef1234567890 --instance-type t2.micro
+echo ${AMI_ID}
+
+SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow-all-sg"|jq '.SecurityGroups[].GroupId'|sed -e 's/"//g')
+
+aws ec2 run-instances \
+--image-id ${AMI_ID} \
+--security-group-ids ${SG_ID} \
+--instance-type t3.micro \
+--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]" \
+--instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
