14a0ea7ca0b238cf709ea39b946a00b657ba0862
shashang13 <shashang13@gmail.com>
2022-11-18 04:59:42 +0530

    create-ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -11,10 +11,11 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 echo ${AMI_ID}
 
 SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow-all-sg"|jq '.SecurityGroups[].GroupId'|sed -e 's/"//g')
+echo ${SG_ID}
 
 aws ec2 run-instances \
 --image-id ${AMI_ID} \
 --security-group-ids ${SG_ID} \
 --instance-type t3.micro \
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]" \
+--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
 --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"
