b89d2ebfeea6ecdfd55f843bde0c4efe7ce4297b
shashang13 <shashang13@gmail.com>
2022-11-18 05:40:59 +0530

    create-ec2

--- create-ec2.sh
+++ create-ec2.sh
@@ -8,6 +8,19 @@ COMPONENT=$1
 
 HOSTED_ZONE_ID="Z03359161XEIHWG682X1"
 
+createEc2(){
+  PRIVATE_IP=$(aws ec2 run-instances \
+  --image-id ${AMI_ID} \
+  --security-group-ids ${SG_ID} \
+  --instance-type t3.micro \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+  |jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+
+  sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/route53.json
+
+  aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file:///tmp/route53.json
+}
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice"|jq '.Images[].ImageId'|sed -e 's/"//g')
 echo ${AMI_ID}
@@ -15,15 +28,12 @@ echo ${AMI_ID}
 SG_ID=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=allow-all-sg"|jq '.SecurityGroups[].GroupId'|sed -e 's/"//g')
 echo ${SG_ID}
 
-PRIVATE_IP=$(aws ec2 run-instances \
---image-id ${AMI_ID} \
---security-group-ids ${SG_ID} \
---instance-type t3.micro \
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
---instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
-|jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
-
-sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/route53.json
-
-aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --change-batch file:///tmp/route53.json
-
+if [ "$1" == "all" ]; then
+  for component in cart catalogue frontend redis mysql mongodb payment rabbitmq shipping user dispatch
+  do
+    COMPONENT=${component}
+    createEc2
+  done
+else
+  createEc2
+fi
