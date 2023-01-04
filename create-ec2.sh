55ea0c77dfb68049531439ee679fb4bb9291842d
shashang13 <shashang13@gmail.com>
2023-01-04 11:53:04 +0530

    Update user data for ec2.

--- create-ec2.sh
+++ create-ec2.sh
@@ -15,6 +15,7 @@ createEc2(){
   --instance-type t3.micro \
   --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" \
   --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
+  --user-data "file://ec2userdata.sh ${COMPONENT}" \
   |jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json > /tmp/route53.json
